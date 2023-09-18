686e03294db718d5c4e89c8bda71e1a9fafd1e0a
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2023-09-18 22:55:06 +0300

    update

---
+++ get-python-win.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+set ZIP_URL=https://github.com/Randy420Marsh/cpython/releases/download/v3.10.13/python-3.10.13-embed-amd64.zip
+set ZIP_FILENAME=python-3.10.13-embed-amd64.zip
+set EXTRACT_FOLDER=python-3.10.13-embed-amd64
+set CHECKSUM=b455d386b29ae9d93f906ce0ef1aeef1a64eb32348d678a0ee466437fa620a62
+
+if not exist "%ZIP_FILENAME%" (
+    echo File "%ZIP_FILENAME%" not found. Downloading...
+    powershell -command "& {Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%ZIP_FILENAME%'}"
+)
+
+if exist "%ZIP_FILENAME%" (
+    echo Verifying checksum...
+    powershell -command "& {Get-FileHash -Path '%ZIP_FILENAME%' -Algorithm SHA256 | ForEach-Object {if ($_.Hash -eq '%CHECKSUM%') {exit 0} else {exit 1}}}"
+    if %errorlevel% equ 0 (
+        echo Checksum verified. Extracting...
+        powershell -command "& {Expand-Archive -Path '%ZIP_FILENAME%' -DestinationPath '%EXTRACT_FOLDER%'}"
+        echo Extraction complete.
+    ) else (
+        echo Checksum mismatch. Deleting the file.
+        del "%ZIP_FILENAME%"
+    )
+)
+
+endlocal
+pause
---
+++ start.bat
@@ -0,0 +1,41 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set "PYTHON=C:\Python-3.10\PCbuild\amd64\python.exe"
+
+echo "Launching..."
+
+cd %CD%
+
+REM ##########################################
+
+set "SD_ROOT_PATH=C:\AI\stable_diffusion_models_and_vae\models"
+
+REM ##########################################
+
+set "USER=%USERNAME%"
+
+echo Current User = %USER%
+
+echo SD models root path = %SD_ROOT_PATH%
+
+set SAFETENSORS_FAST_GPU=1
+
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
+
+call .\venv\scripts\activate.bat
+
+::set python="\\Python-3.10.12\\python.exe"
+
+::set python3="\\Python-3.10.12\\python.exe"
+
+echo "venv activated"
+python --version
+
+::#export controlnet_dir="$SD_ROOT_PATH/models/ControlNet"
+::#export controlnet_annotator_models_path="$SD_ROOT_PATH/models/ControlNet/annotator/models"
+
+python -s main.py --dont-upcast-attention --port 4434 --normalvram --use-pytorch-cross-attention
+
+pause
