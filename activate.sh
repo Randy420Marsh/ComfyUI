662825f17717010e1b1586d3eb8edd597cd9120d
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2026-03-21 03:26:04 +0200

    update

--- activate.sh
+++ activate.sh
@@ -5,6 +5,11 @@ unset LD_LIBRARY_PATH
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+export BNB_CUDA_VERSION=130
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-13.0/lib64
+
+export UV_LINK_MODE=copy
+
 source "$SCRIPT_DIR/.venv/bin/activate"
 
 echo "Activated clean CUDA environment."
